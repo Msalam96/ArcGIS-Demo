a299d38fd80d3368a3aefdfb04d1c67b6211be10
Msalam96 <34826407+Msalam96@users.noreply.github.com>
2019-12-27 09:29:11 -0500

    cont work on auth

--- ArcGIS-Demo/View/LoginViewController.swift
+++ ArcGIS-Demo/View/LoginViewController.swift
@@ -9,39 +9,57 @@
 import UIKit
 import ArcGIS
 
-class LoginViewController:UIViewController {
+class LoginViewController:UIViewController, AGSAuthenticationManagerDelegate {
 //   private let credential = AGSCredential(user: "brandontod97", password:"wyrsuz-wyhwo6-Wefmyw")
      private let portalURL = URL(string: "https://www.arcgis.com")!
      private let clientID = "e6Lfvlw05UccMiiU"
      private let redirectURL = "ArcGIS-Demo://auth"
-     var authenticationManager:AGSAuthenticationManager = AGSAuthenticationManager.shared()
+     private let credential = AGSCredential(user: "brandontod97", password: "wyrsuz-wyhwo6-Wefmyw")
+     var authenticationManager = AGSAuthenticationManager.shared()
+     var activeChallenge:AGSAuthenticationChallenge?
 
+     //private let portal = AGSPortal(url: URL(string: "https://www.arcgis.com")!, loginRequired: true)
 
-     private let portal = AGSPortal(url: URL(string: "https://www.arcgis.com")!, loginRequired: true)
      
      override func viewDidLoad() {
           super.viewDidLoad()
           view.backgroundColor = .red
-          self.portal.credential = AGSCredential(user: "brandontod97", password: "wyrsuz-wyhwo6-Wefmyw")
-          
-           self.portal.load() {[weak self] (error) in
-              if let error = error {
-                  print(error)
-                  return
-              }
-                   // check the portal item loaded and print the modified date
-              if self?.portal.loadStatus == AGSLoadStatus.loaded {
-                  let fullName = self?.portal.user?.fullName
-                  print(fullName!)
-              }
-          }
-          
+          self.authenticationManager.delegate = self
+          self.setupOauth()
+          //activeChallenge? = AGSAuthenticationChallengeType.usernamePassword
+          print("credential[ username: \(credential.username ?? "UNKNOWN") -- password: \(credential.password ?? "UNKNOWN")")
+          activeChallenge?.continueWithDefaultHandling()
+          print(activeChallenge == nil)
+//          activeChallenge?.continue(with: credential)
+     }
+     
+     func setupOauth(){
           let oauthConfiguration = AGSOAuthConfiguration(portalURL: NSURL(string: "https://www.arcgis.com")! as URL, clientID: clientID, redirectURL: redirectURL)
           authenticationManager.oAuthConfigurations.add(oauthConfiguration)
-
-     
      }
-    
+     
+     func authenticationManager(_ authenticationManager: AGSAuthenticationManager, didReceive challenge: AGSAuthenticationChallenge) {
+          activeChallenge = challenge
+          print(activeChallenge == nil)
+          activeChallenge?.continue(with: credential)
+          //challenge.continueWithDefaultHandling()
+     }
+     
+     func authenticationTypeFromChallengeType (challengeType:AGSAuthenticationChallengeType) -> String {
+
+         switch challengeType {
+         case AGSAuthenticationChallengeType.oAuth:
+             return "OAuth"
+         case AGSAuthenticationChallengeType.usernamePassword:
+             return "ArcGIS Token"
+         case AGSAuthenticationChallengeType.untrustedHost:
+             return "HTTP"
+         case AGSAuthenticationChallengeType.clientCertificate:
+             return "PKI"
+         default:
+             return "unknown"
+         }
+     }
 }
 
     
