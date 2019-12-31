1e9eba2bdb0acb2e26b6508d520f0370ab28890c
Msalam96 <34826407+Msalam96@users.noreply.github.com>
2019-12-31 12:02:02 -0500

    Seperated view into View class

---
+++ ArcGIS-Demo/View/LoginView.swift
@@ -0,0 +1,70 @@
+//
+//  LoginView.swift
+//  ArcGIS-Demo
+//
+//  Created by Mohammed Salam on 12/31/19.
+//  Copyright © 2019 BMS. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class LoginView:UIView {
+    
+    let loginContentView:UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    let usernameTextField:UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.placeholder = "Username"
+        return textField
+    }()
+    
+    let passwordTextField:UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.placeholder = "Password"
+        return textField
+    }()
+    
+    let loginButton:UIButton = {
+        let btn = UIButton(type:.system)
+        btn.backgroundColor = .systemTeal
+        btn.setTitle("Login", for: .normal)
+        btn.tintColor = .white
+        btn.layer.cornerRadius = 5
+        btn.clipsToBounds = true
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    func setUpAutoLayout(){
+        usernameTextField.topAnchor.constraint(equalTo:loginContentView.topAnchor, constant:40).isActive = true
+        usernameTextField.heightAnchor.constraint(equalToConstant:50).isActive = true
+        usernameTextField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        usernameTextField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        
+        passwordTextField.topAnchor.constraint(equalTo:usernameTextField.bottomAnchor, constant:20).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant:50).isActive = true
+        passwordTextField.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        passwordTextField.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        
+        loginButton.topAnchor.constraint(equalTo:passwordTextField.bottomAnchor, constant:20).isActive = true
+        loginButton.leftAnchor.constraint(equalTo:loginContentView.leftAnchor, constant:20).isActive = true
+        loginButton.rightAnchor.constraint(equalTo:loginContentView.rightAnchor, constant:-20).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant:50).isActive = true
+    }
+    
+}
---
+++ ArcGIS-Demo/View/LoginViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewModel.swift
+//  ArcGIS-Demo
+//
+//  Created by Mohammed Salam on 12/31/19.
+//  Copyright © 2019 BMS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct LoginViewModel {
+    
+//    let loginView = LoginView()
+//    
+//    func buttonAction(sender: UIButton!) {
+//        if loginView.usernameTextField.text?.isEmpty ?? false || loginView.passwordTextField.text?.isEmpty ?? false{
+//             let alert = UIAlertController(title: "Error", message: "Username and Password Must Be Filled", preferredStyle: UIAlertController.Style.alert)
+//             alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+//            loginView.loginContentView.present(alert, animated: true, completion: nil)
+//        } else {
+//             print("its haddening")
+//             let credentials = AGSCredential(user: usernameTextField.text!, password: passwordTextField.text!)
+//             auth.activeChallenge?.continue(with: credentials)
+//        }
+//    }
+}
