5e87757abeb635bfb7a6547e58da7568957fdf31
Sarthak Thakur <Sarthak@Nareshs-MacBook-Pro.local>
2020-01-02 10:54:33 -0500

    profile stuff done

---
+++ ArcGIS-Demo/View/UserDisplayViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  UserDisplayViewModel.swift
+//  ArcGIS-Demo
+//
+//  Created by Naresh on 1/2/20.
+//  Copyright © 2020 BMS. All rights reserved.
+//
+import UIKit
+import ArcGIS
+
+class UserDisplayViewModel
+{
+    let userPortal: AGS
+   
+    init(userPortal: AGS)
+    {
+        self.userPortal = userPortal
+    }
+    
+    func displayUserInfo() -> [String?]
+    {
+        var toDisplay: String?
+        let displayArray: [String?] = [("Name: \(self.userPortal.portal.user?.fullName?.description ?? "")"), "Email: \(self.userPortal.portal.user?.email?.description ?? "")", ("Date Created: \(self.userPortal.portal.user?.created?.description ?? "")"),
+                                       ("User Name: \(self.userPortal.portal.user?.username?.description ?? "")")
+        ]
+        
+        
+     return displayArray
+    }
+    
+    
+}
